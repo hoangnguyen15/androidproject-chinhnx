6eaac9cdeb5d437e6aff86e4488eccd422c0868f
chinhnx2712@gmail.com <chinhnx2712@gmail.com@8b9ff4ec-0ac2-6a04-35a6-339287eff226>
2013-04-23 08:52:18 +0000

    create splash screen

---
+++ trunk/android/IC_LichVanNien/src/com/icsoft/ic_lichvannien/Splash.java
@@ -0,0 +1,24 @@
+package com.icsoft.ic_lichvannien;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+	private static final int SPLASH_DURATION = 1000; 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				finish();
+				startActivity(new Intent(Splash.this,Main.class));
+			}
+		},SPLASH_DURATION);
+	}
+}
