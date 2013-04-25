58a0771d3f5343c05d25bb9b78ccf0780c6ff905
chinhnx2712@gmail.com <chinhnx2712@gmail.com@8b9ff4ec-0ac2-6a04-35a6-339287eff226>
2013-04-25 03:10:59 +0000

    dayview layout

---
+++ trunk/android/IC_LichVanNien/src/com/icsoft/ic_lichvannien/Dayview.java
@@ -0,0 +1,13 @@
+package com.icsoft.ic_lichvannien;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dayview extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dayview);
+	}
+}
