d9cacfa63a7cc3ece217183fd72e9f13a064ca0c
chinhnx2712@gmail.com <chinhnx2712@gmail.com@8b9ff4ec-0ac2-6a04-35a6-339287eff226>
2013-04-26 02:04:58 +0000

    create view change date

--- trunk/android/IC_LichVanNien/src/com/icsoft/ic_lichvannien/ChangeDate.java
+++ trunk/android/IC_LichVanNien/src/com/icsoft/ic_lichvannien/ChangeDate.java
@@ -1,13 +1,30 @@
 package com.icsoft.ic_lichvannien;
 
+import java.lang.reflect.Method;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 public class ChangeDate extends  Activity {
+	DatePicker dpSolar,dpLunar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.changedate);
+		dpSolar = (DatePicker)findViewById(R.id.dpSolar);
+		dpLunar = (DatePicker)findViewById(R.id.dpLunar);
+		
+		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= 11) {
+          try {
+            Method m = dpSolar.getClass().getMethod("setCalendarViewShown", boolean.class);
+            m.invoke(dpSolar, false);
+            m = dpLunar.getClass().getMethod("setCalendarViewShown", boolean.class);
+            m.invoke(dpLunar, false);
+          }
+          catch (Exception e) {} // eat exception in our case
+        }
 	}
 }
